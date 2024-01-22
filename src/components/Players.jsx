f003c1165d0f8d46b2e50cde404bccdd754c5038
Ennypitan Adekoya <ennypitan@gmail.com>
2024-01-22 11:32:33 +0100

    updated

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
 function Players({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    // setIsEditing({ isEditing: true });
+    setIsEditing(true);
+    console.log(isEditing);
+  };
+
   return (
     <>
       <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+          {isEditing === false ? (
+            <span className="player-name">{name}</span>
+          ) : (
+            <input type="text" required></input>
+          )}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>Edit</button>
       </li>
     </>
   );
