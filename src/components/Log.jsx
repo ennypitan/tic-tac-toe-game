f2af016df41e2dcf01604a214a6d5401264ab7d1
Ennypitan Adekoya <ennypitan@gmail.com>
2024-01-31 16:30:53 +0100

    updated

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,13 @@
-function Log() {
+function Log({ turns }) {
   return (
     <>
-      <ol id="log">log</ol>
+      <ol id="log">
+        {turns.map((turn) => (
+          <li key={`${turn.square.row}${turn.square.col}`}>
+            {turn.player} selected {turn.square.row}, {turn.square.col}
+          </li>
+        ))}
+      </ol>
     </>
   );
 }
