cdb399be245737d114fb0c689d57475553980652
Ennypitan Adekoya <ennypitan@gmail.com>
2024-02-09 23:30:20 +0100

    updated

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,11 +3,11 @@ import styles from "./gameOver.module.css";
 function GameOver({ winner, onRestart }) {
   return (
     <div className={styles["game-over"]}>
-      <h2 className={styles.h2}>Game Over!!</h2>
+      <h2 className={styles["g-over-title"]}>Game Over!!</h2>
       {winner && <p>{winner} Won!</p>}
       {!winner && <p>It's a draw!</p>}
-      <p className={styles.p}>
-        <button className={styles.Button} onClick={onRestart}>
+      <p className={styles["gameOver-p"]}>
+        <button className={styles["gameOver-button"]} onClick={onRestart}>
           Rematch
         </button>
       </p>
