da5e628a68657c1f1c6c287aa7b30501dae7c3f5
Ennypitan Adekoya <ennypitan@gmail.com>
2024-02-12 17:09:42 +0100

    updated

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
